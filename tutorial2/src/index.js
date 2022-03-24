5e1940a1c675a5c90cd6fff98c189e98b5f2c2bd
sameeramin2015 <93704415+sameeramin2015@users.noreply.github.com>
2022-03-24 11:28:00 +0100

    Add files via upload

---
+++ tutorial2/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+/*
+npx create-react-app tutorial2
+cd tutorial2
+npm install react-bootstrap bootstrap
+npm install sass
+*/
+
