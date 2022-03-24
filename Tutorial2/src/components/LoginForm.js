d2e820179f399de0120714e7a411bfa5de56688e
sameeramin2015 <93704415+sameeramin2015@users.noreply.github.com>
2022-03-24 10:47:34 +0100

    Add files via upload

---
+++ Tutorial2/src/components/LoginForm.js
@@ -0,0 +1,27 @@
+import { Button, Form } from "react-bootstrap";
+import React from "react";
+
+export default function LoginForm({submitHandler, formHandler}) {
+  return (
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" name="email" onChange={formHandler} placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" name="password" onChange={formHandler} placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <Button variant="primary" onClick={submitHandler} type="submit">
+                        Submit
+                    </Button>
+                </Form>
+  )
+}
