eaf1af10f949a17f7e8402271e7f478a485de3a1
sameeramin2015 <93704415+sameeramin2015@users.noreply.github.com>
2022-03-24 13:44:29 +0100

    Update App.js

--- tutorial3/src/App.js
+++ tutorial3/src/App.js
@@ -3,7 +3,9 @@ import {useEffect} from "react";
 import { Button, Container } from 'react-bootstrap';
 function App() {
   // useEffect , useState , useRef , useContext >> react hooks
-  const [posts, setPosts] = useState([]);  // posts and setPost are methods of useState // we saved posts json data in here
+  const [posts, setPosts] = useState([]); // posts and setPost are methods of useState // we saved posts json data in here
+  const [IndexNumber, setIndexNumber] = useState(1); 
+  const [singlePost, setSinglePosts] = useState({}); 
   useEffect (() => {
     console.log ('hello world')
     fetch('https://jsonplaceholder.typicode.com/posts')
@@ -14,12 +16,45 @@ function App() {
     })
     .catch(err=> console.log(err))
   },[])
+  //https://jsonplaceholder.typicode.com/posts/1
+  const nextPost = (e)=> {
+    e.preventDefault()
+    console.log(IndexNumber)
+    setIndexNumber(IndexNumber+1)
+    // {/*fetch('https://jsonplaceholder.typicode.com/posts/'+IndexNumber)*/}
+    fetch(`https://jsonplaceholder.typicode.com/posts/${IndexNumber}`)
+    .then(res=>res.json())
+    .then (data=>{
+      console.log(data)
+      setSinglePosts(data)
+    })
+
+  }
+  const prevPost =(e)=>{
+    e.preventDefault()
+    
+    
+    if (IndexNumber<=0){
+      //false
+      alert('No post found')
+    } else{
+      setIndexNumber(IndexNumber-1)
+      // {/*fetch('https://jsonplaceholder.typicode.com/posts/'+IndexNumber)*/}
+    fetch(`https://jsonplaceholder.typicode.com/posts/${IndexNumber}`)
+    .then(res=>res.json())
+    .then (data=>{
+      console.log(data)
+      setSinglePosts(data)
+    })
+  }
+    }
+    
   return (
     
       <Container className="text-center mt-5 bg-primary p-5 ">
-        <p>Singel Post title</p>
-        <Button variant={'danger'} className="mx-1">Previous Post</Button>
-        <Button variant={'warning'} >Next Post</Button>
+        <p>{singlePost.title}</p>
+        <Button variant={'danger'} className="mx-1 font-weight-bold" onClick={prevPost}>Previous Post</Button>
+        <Button variant={'warning'} onClick={nextPost}>Next Post</Button>
       
         {/*
         posts.map((post, index)=>(
